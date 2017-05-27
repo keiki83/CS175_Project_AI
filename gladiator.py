65efbb90627528c69a01c925cdb121ed98f4494d
Jose M. Catalasan <jmcatalasan@gmail.com>
2017-05-26 17:23:00 -0700

    converted some indentation from spaces to tabs. cleaned up main loop and lookAtNearestEntity

--- gladiator.py
+++ gladiator.py
@@ -20,42 +20,49 @@ MOB_TYPE = "Zombie"
 
 # sarsa function
 def SARSA():
-    # alpha = learning rate (0-1)   0: learn nothing, 1: recent only
-    # gamma = discount factor (0-1) 0: current rewards only, 1: long-term reward
+	# alpha = learning rate (0-1)   0: learn nothing, 1: recent only
+	# gamma = discount factor (0-1) 0: current rewards only, 1: long-term reward
 
-    # Initialize Q(s,a) arbitrarily
-    # Repeat (for each episode):
-        # Initalize s
-        # Repeat (for each step of episode):
-            # Choose a from s using policy derived from Q (e.g., epsilon-greedy)
-            # Take action a, observe r, s'
-            # Q[s,a] = q[s,a] + alpha*(r + gamma*Q[s',a'] - Q[s,a])
-            # s = s'
-            # a= a'
-        # until s is terminal
-    return
+	# Initialize Q(s,a) arbitrarily
+	# Repeat (for each episode):
+		# Initalize s
+		# Repeat (for each step of episode):
+			# Choose a from s using policy derived from Q (e.g., epsilon-greedy)
+			# Take action a, observe r, s'
+			# Q[s,a] = q[s,a] + alpha*(r + gamma*Q[s',a'] - Q[s,a])
+			# s = s'
+			# a= a'
+		# until s is terminal
+	return
 
 #helper functions
 def findUs(entities):
-    for ent in entities:
-        if ent.name == MOB_TYPE:
-            continue
-        else:
-            return ent
+	for ent in entities:
+		if ent.name == MOB_TYPE:
+			continue
+		else:
+			return ent
 
 def lookAtNearestEntity(entities):
-    us = findUs(entities)
-    closestEntity = 0
-    entityDistance = sys.float_info.max
-    for i,ent in enumerate(entities):
-        if ent.name == MOB_TYPE:
-            #check distance from us and get the lowest
-            dist = (ent.x - us.x)*(ent.x - us.x) + (ent.z - us.z)*(ent.z - us.z)
-            if(dist < entityDistance):
-                closestEntity = i
-                entityDistance = dist
-    best_yaw = math.degrees(math.atan2(entities[closestEntity].z - us.z, entities[closestEntity].x - us.x)) - 90
-    return best_yaw
+	us = findUs(entities)
+	current_yaw = us.yaw
+	closestEntity = 0
+	entityDistance = sys.float_info.max
+	for i,ent in enumerate(entities):
+		if ent.name == MOB_TYPE:
+			#check distance from us and get the lowest
+			dist = (ent.x - us.x)*(ent.x - us.x) + (ent.z - us.z)*(ent.z - us.z)
+			if(dist < entityDistance):
+				closestEntity = i
+				entityDistance = dist
+	best_yaw = math.degrees(math.atan2(entities[closestEntity].z - us.z, entities[closestEntity].x - us.x)) - 90
+	difference = best_yaw - current_yaw;
+	while difference < -180:
+		difference += 360;
+	while difference > 180:
+		difference -= 360;
+	difference /= 180.0;
+	return difference
 
 
 
@@ -67,14 +74,14 @@ sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immedi
 
 agent_host = MalmoPython.AgentHost()
 try:
-    agent_host.parse( sys.argv )
+	agent_host.parse( sys.argv )
 except RuntimeError as e:
-    print 'ERROR:',e
-    print agent_host.getUsage()
-    exit(1)
+	print 'ERROR:',e
+	print agent_host.getUsage()
+	exit(1)
 if agent_host.receivedArgument("help"):
-    print agent_host.getUsage()
-    exit(0)
+	print agent_host.getUsage()
+	exit(0)
 
 #load mission from file
 mission_file = './arena1.xml'
@@ -88,25 +95,25 @@ my_mission_record = MalmoPython.MissionRecordSpec()
 # Attempt to start a mission:
 max_retries = 3
 for retry in range(max_retries):
-    try:
-        agent_host.startMission( my_mission, my_mission_record )
-        break
-    except RuntimeError as e:
-        if retry == max_retries - 1:
-            print "Error starting mission:",e
-            exit(1)
-        else:
-            time.sleep(2)
+	try:
+		agent_host.startMission( my_mission, my_mission_record )
+		break
+	except RuntimeError as e:
+		if retry == max_retries - 1:
+			print "Error starting mission:",e
+			exit(1)
+		else:
+			time.sleep(2)
 
 # Loop until mission starts:
 print "Waiting for the mission to start ",
 world_state = agent_host.getWorldState()
 while not world_state.has_mission_begun:
-    sys.stdout.write(".")
-    time.sleep(0.1)
-    world_state = agent_host.getWorldState()
-    for error in world_state.errors:
-        print "Error:",error.text
+	sys.stdout.write(".")
+	time.sleep(0.1)
+	world_state = agent_host.getWorldState()
+	for error in world_state.errors:
+		print "Error:",error.text
 
 print
 print "Mission running ",
@@ -121,52 +128,40 @@ current_life = 0
 
 # Loop until mission ends:
 while world_state.is_mission_running:
-    agent_host.sendCommand("attack 1")
-    world_state = agent_host.getWorldState()
-    if world_state.number_of_observations_since_last_state > 0:
-        #this is where the rewards are counted and the policy is updated
-        current_reward = 0
-        msg = world_state.observations[-1].text
-        ob = json.loads(msg)
+	agent_host.sendCommand("attack 1")
+	world_state = agent_host.getWorldState()
+	if world_state.number_of_observations_since_last_state > 0:
+		#this is where the rewards are counted and the policy is updated
+		current_reward = 0
+		msg = world_state.observations[-1].text
+		ob = json.loads(msg)
 
-        if "Life" in ob:
-            life = ob[u'Life']
-            if life < current_life:
-                agent_host.sendCommand("chat aaaaaaaaargh!!!")
-                #do something with rewards
-            current_life = life
-        if "entities" in ob:
-            entities = [EntityInfo(**k) for k in ob["entities"]]
-            current_yaw = findUs(entities).yaw
-            #for ent in entities:
-                # if ent.life > 10:
-            #store hp of entities in list X
-            #search for mob entities that have taken damage
-            #do stuff with rewards based on entities here
-            #store hp of entities in list Y
+		if "Life" in ob:
+			life = ob[u'Life']
+			if life < current_life:
+				agent_host.sendCommand("chat aaaaaaaaargh!!!")
+				#do something with rewards
+			current_life = life
+		if "entities" in ob:
+			entities = [EntityInfo(**k) for k in ob["entities"]]
 
-            # turn towards the nearest zombie
-            best_yaw = lookAtNearestEntity(entities);
-            difference = best_yaw - current_yaw;
-            #difference = 120 - current_yaw
-            while difference < -180:
-                difference += 360;
-            while difference > 180:
-                difference -= 360;
-            difference /= 180.0;
-            agent_host.sendCommand("turn " + str(difference))
-        #by here we know the game state
-        #choose action based on policy
-            #go through all actions, see where it places you in the map and give a score based on enemy locations and wall locations
-            #choose whichever scores highest
-        #update policy
-        #change old state to new state
-        #change old action to new action
-    time.sleep(0.02)
+		#by here we know the game state
+		#Sarsa starts here
+
+		#for ent in entities:
+			#check entities
+
+		# actions carries out here
+		# turn towards the nearest zombie
+		difference = lookAtNearestEntity(entities);
+		agent_host.sendCommand("turn " + str(difference))
+
+	time.sleep(0.02) #end of while loop
+
 
 # mission has ended.
 for error in world_state.errors:
-    print "Error:",error.text
+	print "Error:",error.text
 
 print
 print "Mission ended"
